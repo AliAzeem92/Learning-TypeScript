3c10057735c7b70aad08737776aeadb523ddfc41
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-04 00:17:44 +0500

    Terminal Output Problem Resolve

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var userName = "Ali Azeem";
+var userName2 = "Ali Azeem";
+var userName3 = "Ali Azeem";
+var helpFriend = "By Muhammad Ahamad";
 console.log(userName);
+console.log(userName2);
+console.log(userName3);
+console.log(helpFriend);
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 var userName:string = "Ali Azeem";
 var userName2:string = "Ali Azeem";
+var userName3:string = "Ali Azeem";
+
+var helpFriend:string = "By Muhammad Ahamad"
 
 console.log(userName);
 console.log(userName2);
+console.log(userName3);
+
+console.log(helpFriend);
